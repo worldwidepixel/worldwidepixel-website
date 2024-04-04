d5ff64cc2c8c9bc9c447c00ac71814975fe9e9c1
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-04-03 21:16:07 -0700

    pixelpack update

---
+++ pages/pixelpack.vue
@@ -0,0 +1,119 @@
+<template>
+
+<div class="flex flex-col gap-4">
+		<span class="font-bold text-[4rem] brand-gradient">pixelpack</span>
+    <span class="text-2xl"> by worldwidepixel </span>
+		<h2 class="text-lg">for 1.18.x, 1.19.x and 1.20.x</h2>
+
+    <div class="flex flex-col gap-8">
+
+      <hr>
+
+      <div class="flex flex-col md:flex-row gap-4 justify-between">
+
+        <div class="flex flex-col text-4xl gap-4">
+
+          <span class="font-bold text-4xl"> download latest for: </span>
+
+          <a href="https://www.mediafire.com/file/j785tpb7qaz0jyt/PixelPack_%255B1.20.X%255D_v3.1.zip/file"> <span class="underline text-5xl"> 1.20.x v3.1 </span> </a>
+          <a href="https://www.mediafire.com/file/14cjr8tj6c3sbhr/PixelPack_%255B1.19%255D_v2.2.zip/file"> <span class="underline text-5xl"> 1.19.x v2.2 </span> </a>
+          <a href="https://www.mediafire.com/file/pd3nyx448fugxk5/PixelPack_%255B1.18.2%255D_v2.1.zip/file"> <span class="underline text-5xl"> 1.18.2 v2.1 </span> </a>
+
+        </div>
+
+        <div>
+
+          <img class="w-[25rem] max-w-full rounded-3xl" src="/img/pixelpack.png">
+
+        </div>
+
+      </div>
+
+      <span class="font-light text-4xl"> legacy versions </span>
+
+      <div class="flex flex-col md:flex-row gap-32">
+
+        <div class="flex flex-col gap-4">
+
+          <div class="flex flex-col gap-4">
+
+            <span class="text-4xl"> 1.20.x </span>
+
+            <a href="https://www.mediafire.com/file/j785tpb7qaz0jyt/PixelPack_%255B1.20.X%255D_v3.1.zip/file"> <span class="font-bold text-2xl underline"> latest v3.1 </span> </a>
+
+          </div>
+
+          <div class="flex flex-col gap-4">
+
+            <span class="text-4xl"> 1.19.4 </span>
+
+            <a href="https://www.mediafire.com/file/14cjr8tj6c3sbhr/PixelPack_%255B1.19%255D_v2.2.zip/file"> <span class="font-bold text-2xl underline"> latest v2.2 </span> </a>
+            <a href="https://www.mediafire.com/file/5gt28i9eb6tfrmn/PixelPack_%255B1.19%255D_v2.1.zip/file"> <span class="font-bold text-2xl underline"> 1.19.4 v2.1 </span> </a>
+
+          </div>
+
+          <div class="flex flex-col gap-4">
+
+            <span class="text-4xl"> 1.17.1 </span>
+
+            <a href="https://www.mediafire.com/file/mcrwqpvmgwonteq/PixelPack_%255B1.17.1%255D_v2.2.zip/file"> <span class="font-bold text-2xl underline"> latest v2.2 </span> </a>
+            <a href="https://www.mediafire.com/file/tkuto14cr3mvl2j/PixelPack_%255B1.17.1%255D_v2.0.zip/file"> <span class="font-bold text-2xl underline"> 1.17.1 v2.0 </span> </a>
+            <a href="https://www.mediafire.com/file/ry967wzmkus587o/PixelPack_%255B1.17.1%255D_v1.9.zip/file"> <span class="font-bold text-2xl underline"> 1.17.1 v1.9 </span> </a>
+            <a href="https://www.mediafire.com/file/k7lnkj4p6i6oxi1/PixelPack_%255B1.17%255D_v1.6.zip/file"> <span class="font-bold text-2xl underline"> 1.17.1 v1.6 </span> </a>
+            <a href="https://www.mediafire.com/file/rtmrblg4rz2msyz/PixelPack_%255B1.17%255D_v1.5.zip/file"> <span class="font-bold text-2xl underline"> 1.17.1 v1.5 </span> </a>
+            <a href="https://www.mediafire.com/file/j7mg4qne4ah6boq/PixelPack_%255B1.17%255D_v1.4.zip/file"> <span class="font-bold text-2xl underline"> 1.17.1 v1.4 </span> </a>
+            <a href="https://www.mediafire.com/file/zqj6btxmfz5u2wc/PixelPack_%255B1.17%255D_v1.3.zip/file"> <span class="font-bold text-2xl underline"> 1.17.1 v1.3 </span> </a>
+
+          </div>
+
+        </div>
+
+        <div class="flex flex-col gap-4">
+
+          <div class="flex flex-col gap-4">
+
+            <span class="text-4xl"> 1.18.2 </span>
+
+            <a href="https://www.mediafire.com/file/pd3nyx448fugxk5/PixelPack_%255B1.18.2%255D_v2.1.zip/file"> <span class="font-bold text-2xl underline"> latest v2.1 </span> </a>
+
+          </div>
+
+          <div class="flex flex-col gap-4">
+
+            <span class="text-4xl"> 1.16.5 </span>
+
+            <a href="https://www.mediafire.com/file/gnr8xsy4bwjbcc4/PixelPack_%255B1.16.5%255D_v1.4.zip/file"> <span class="font-bold text-2xl underline"> latest v1.4 </span> </a>
+            <a href="https://www.mediafire.com/file/fdyb2qnx1ehd5nc/PixelPack_%255B1.16.5%255D_v1.3.zip/file"> <span class="font-bold text-2xl underline"> 1.16.5 v1.3 </span> </a>
+            <a href="https://www.mediafire.com/file/at4dzg9c9oe8fse/PixelPack_%255B1.16.5%255D.zip/file"> <span class="font-bold text-2xl underline"> 1.16.5 v1.1 </span> </a>
+
+          </div>
+
+          <div class="flex flex-col gap-4">
+
+            <span class="text-4xl"> 1.8.9 </span>
+
+            <a href="https://www.mediafire.com/file/92ezh739d4g4wpz/PixelPack_%255B1.8.9%255D_v1.4.zip/file"> <span class="font-bold text-2xl underline"> latest v1.4 </span> </a>
+            <a href="https://www.mediafire.com/file/g2cavaqg7umlio5/PixelPack_%255B1.8.9%255D_v1.3.zip/file"> <span class="font-bold text-2xl underline"> 1.8.9 v1.3 </span> </a>
+            <a href="https://www.mediafire.com/file/lgf07we4986fbon/PixelPack_%255B1.8.9%255D.zip/file"> <span class="font-bold text-2xl underline"> 1.8.9 v1.0 </span> </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <span> PixelPack for 1.8.9, 1.16.5, 1.17.1, and 1.19.4 are no longer supported.</span>
+
+    </div>
+
+</div>
+
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'worldwidepixel - pixelpack',
+  description: "I make things. On the internet.",
+  ogImage: "/img/icon512.png",
+})
+</script>
