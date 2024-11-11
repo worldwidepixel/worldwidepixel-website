c20577c85ef7a2708ae28e4834b1463f1dee7bf1
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-11-11 12:12:04 -0800

    Initial V3

--- components/projectBadge.vue
+++
@@ -1,9 +0,0 @@
-<template>
-
-    <span class="w-fit text-sm bg-[var(--brand-bright)] p-2 rounded-xl select-none text-[color:var(--brand-colour)]">
-        
-        <slot />
-    
-    </span>
-
-</template>
--- types/gradient.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '~/public/scripts/Gradient.js' {
-    export class Gradient {
-      initGradient(selector: string): Gradient;
-      // Add other method declarations if they exist in your class
-    }
-  }
