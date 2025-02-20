499748f12fab8b64b256c9b462b575280dd097f5
worldwidepixel <worldwidepxl@gmail.com>
2025-02-19 22:40:04 -0800

    feat: Music metadata plus plus

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,17 @@
+export type MusicProject = {
+	title: string;
+	slug: string;
+	icon: string;
+	date: string;
+	colour: string;
+	description: string[];
+	links: Link[];
+};
+
+export type Link = {
+	name: string;
+	to: string;
+};
+
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
